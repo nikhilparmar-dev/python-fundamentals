82bb5325f18b114910d491e02a736fb8888f086d
Nikhil Parmar <nikhilparmarofc@gmail.com>
2026-02-03 19:59:17 +0530

    Pushing more python programs

---
+++ Program_8.py
@@ -0,0 +1,13 @@
+# Write a Python Program to Check if a Number is Positive, Negative or Zero.
+# Date : 30/01/2026
+# Author : Nikhil
+
+
+no = int(input("Enter an Number: "))
+
+if no > 0 :
+    print("Positive Number")
+elif no < 0 :
+    print("Negative Number")
+else :
+    print("Number is zero")
